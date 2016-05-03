2589606d8bc656761bcf05cc32b869948d44a5ad
331287556@qq.com <331287556@qq.com>
2016-05-03 21:53:20 +0800

    修改版chartee

---
+++ ios/chartee/CandleViewController.h
@@ -0,0 +1,15 @@
+//  https://github.com/zhiyu/chartee/
+//
+//  Created by zhiyu on 7/11/11.
+//  Copyright 2011 zhiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Chart.h"
+#import <QuartzCore/QuartzCore.h>
+#import "YAxis.h"
+#import "AutoCompleteDelegate.h"
+
+@interface CandleViewController : UIViewController
+
+@end
